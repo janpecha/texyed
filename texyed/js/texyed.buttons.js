a4494431ef96b3ba7b967583539c692dcda71788
Jan Pecha <janpecha@email.cz>
2012-07-26 09:17:26 +0200

    [buttons] oprava - ted uz funguje navazani handleru na click
    Signed-off-by: Jan Pecha <janpecha@email.cz>

--- texyed/js/texyed.buttons.js
+++ texyed/js/texyed.buttons.js
@@ -4,14 +4,15 @@
  * - $('#mytoolbar').teAddButton('Save', function() {...});
  * 
  * @author		Jan Pecha, <janpecha@email.cz>
- * @version		2012-07-25-1
+ * @version		2012-07-26-1
  */
 
 ;(function($){
 	$.extend($.fn, {
 		teAddButton: function(title, handler) {
-			this.append('<span class="ui-button">' + title + '</span>');
-			this.children('ui-button').last().on('click', handler);
+			$('<span class="ui-button">' + title + '</span>')
+				.on('click', handler)
+				.appendTo(this);
 			
 			return this;
 		}
