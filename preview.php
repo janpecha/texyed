1991970eaa0c1653a319cd7a4d01e68e34b6a5b4
Jan Pecha <janpecha@email.cz>
2012-09-28 17:07:19 +0200

    preview.php: updated
    Signed-off-by: Jan Pecha <janpecha@email.cz>

--- preview.php
+++ preview.php
@@ -2,9 +2,11 @@
 	/** Texyed Preview PHP example
 	 *
 	 * @author		Jan Pecha, <janpecha@email.cz>
-	 * @version		2012-07-27-1
+	 * @version		2012-09-28-1
 	 */
-	
+	#undef_function(40); // PHP fatal error
+	#header('x-husto-prisne: a', TRUE, 404); // Page Not Found
+	#sleep(40); // Timeout
 	$text = $_POST['text'];
 	
 	$html = htmlspecialchars($text);
